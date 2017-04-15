3a6216b00c41052f1da386083a327de7d233c4c5
Tuxist <tuxist@googlemail.com>
2017-04-15 12:35:03 +0200

    fixed cmake config

--- src/cmake/httpppConfig.cmake
+++ src/cmake/httpppConfig.cmake
@@ -1,2 +1,11 @@
 include("${CMAKE_CURRENT_LIST_DIR}/httpppTargets.cmake")
 
+get_filename_component(httppp_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+if(EXISTS "${httppp_CMAKE_DIR}/CMakeCache.txt")
+  # In build tree
+  include("${httppp_CMAKE_DIR}/httpppBuildTreeSettings.cmake")
+else()
+  set(httppp_INCLUDE_DIRS "${httppp_CMAKE_DIR}/@CONF_REL_INCLUDE_DIR@")
+endif()
+
+set(httppp_LIBRARIES httppp)
