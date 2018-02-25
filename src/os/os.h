a0c44bbdd1a92057a95b97ea51b7e359e434bf1c
Jan Koester <jan.koester@gmx.net>
2018-02-25 19:20:33 +0100

    initialwork for multithreading

--- src/os/os.h
+++ src/os/os.h
@@ -7,10 +7,12 @@
   #include "Windows/socket.h"
   #include "Windows/inttype.h"
   #include "Windows/mutex.h"
+  #include "Windows/threadpool.h"
 #else
   #include "Unix/socket.h"
   #include "Unix/inttype.h"
   #include "Unix/mutex.h"
+  #include "Unix/threadpool.h"
 #endif
 
 #endif
