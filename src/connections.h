bf1a056f8d26148005a061943213d926e9e5cd31
Jan Koester <tuxist@googlemail.com>
2018-08-20 16:43:53 +0200

    fixed external builds

--- src/connections.h
+++ src/connections.h
@@ -26,7 +26,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *******************************************************************************/
  
 
-#include <config.h>
+#include "config.h"
 #include "exception.h"
 #include "os/os.h"
 
