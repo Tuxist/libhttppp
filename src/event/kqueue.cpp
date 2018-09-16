5e0a22e1036926f41239a5fa03b8bf43dfa1a075
Jan Koester <tuxist@googlemail.com>
2018-09-16 10:20:30 +0200

    fixed label in kqueue

--- src/event/kqueue.cpp
+++ src/event/kqueue.cpp
@@ -196,7 +196,7 @@ void *libhttppp::Event::WorkerThread(void *wrkevent) {
                                 if (kevent(wevent->_Kq, &setEvent, 1, NULL, 0, NULL) == -1) {
                                     httpexception.Error("runeventloop","can't mod  kqueue!");
                                 } else {
-                                    goto ClOSECONNECTION;
+                                    goto CLOSECONNECTION;
                                 }
                             }
                         }
