c407f2f8681326fd2f00e6ee32bcbd8e80b86295
Jan Koester <jan.koester@gmx.net>
2019-03-03 10:55:58 +0100

    fixed build strutur

--- src/httpd.h
+++ src/httpd.h
@@ -25,7 +25,7 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 *******************************************************************************/
 
-#include "os/event/event.h"
+#include "event.h"
 #include "exception.h"
 
 #ifndef HTTPD_H
