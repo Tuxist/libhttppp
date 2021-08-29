dcb621f98c47158a0ff8b2b5d04139d9da62a1e2
Jan Koester <jan.koester@gmx.net>
2021-08-29 13:26:57 +0200

    some new helpre functions

--- build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jan.koester/Projekte/libhttppp/src
-# Build directory: /home/jan.koester/Projekte/libhttppp/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jan.koester/Projekte/libhttppp/test
-# Build directory: /home/jan.koester/Projekte/libhttppp/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(connection_test "connection_test")
-set_tests_properties(connection_test PROPERTIES  _BACKTRACE_TRIPLES "/home/jan.koester/Projekte/libhttppp/test/CMakeLists.txt;11;add_test;/home/jan.koester/Projekte/libhttppp/test/CMakeLists.txt;0;")
