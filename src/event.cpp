47d02f540b057bd2103827116acd070e3fbbb7cc
Jan Koester <jan.koester@gmx.net>
2021-12-13 07:42:34 +0100

    removed not more needed os folder

--- src/event.cpp
+++ src/event.cpp
@@ -28,7 +28,7 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include <systempp/sysinfo.h>
 #include <systempp/sysconsole.h>
 
-#include <config.h>
+#include "config.h"
 
 #include "connections.h"
 #include "eventapi.h"
